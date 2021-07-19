3019885ba3ab43e4e9649299e889f1e2df02c16f
Xander <xanschwarz@gmail.com>
2021-07-18 18:33:22 -0700

    Initial file and folder commit.

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// ----------------------------------------------- User Story and Acceptance Criteria -----------------------------------------------
+// Want to see the weather outlook for multiple cities.
+
+// Weather dashboard w/form inputs, can search for a city, results will be current and future conditions for that city and city is 
+// added to the search history.
+
+// Current conditions for city shows city name, date, icon representation of conditions, the temp, humidity, wind speed, and UV index.
+
+// UV index displays with color indicating conditions are favorable, moderate, or severe.
+
+// Future conditions are presented as 5-day forecast. Each card has date, icon rep of conditions, the temp, wind speed, and humidity.
+
+// Clicking on city in search history again presents current and future conditions for that city.
+// ----------------------------------------------------------------------------------------------------------------------------------
